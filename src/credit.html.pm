9b6c90b276fdc9bb6b599ce15e8ed166e1635e60
lukejianu <71729102+lukejianu@users.noreply.github.com>
2023-05-28 12:27:58 -0700

    fix: spelling mistake

--- src/credit.html.pm
+++ src/credit.html.pm
@@ -14,7 +14,7 @@
       ◊a[#:href eric-site #:target "_blank"]{Eric Zhang}.
     }
     ◊li{The idea to use Pollen & Racket comes from 
-      ◊a[#:href ryan-site #:target "_blank"]{Ryan Chung} and
+      ◊a[#:href ryan-site #:target "_blank"]{Ryan Jung} and
       ◊a[#:href lucas-site #:target "_blank"]{Lucas Sta Maria}.
       }
   }
