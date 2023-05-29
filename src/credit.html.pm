6ac7ce3af63bad1b1bfb6fe1b3d026bff6ae6d37
lukejianu <71729102+lukejianu@users.noreply.github.com>
2023-05-29 08:57:58 -0700

    fix: broken link

--- src/credit.html.pm
+++ src/credit.html.pm
@@ -2,7 +2,7 @@
  
 ◊(define ryan-site "https://ryanjung.dev/")
 ◊(define lucas-site "https://priime.dev/")
-◊(define eric-site "https://ekzhang.com/")
+◊(define eric-site "https://www.ekzhang.com/")
 
 ◊div[#:class "page-content"]{
   ◊h1[#:class "page-name"]{
