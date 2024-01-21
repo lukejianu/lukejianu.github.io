3b53aba4cb41df1f8a3ede7cf90eae13c8954b1c
lukejianu <71729102+lukejianu@users.noreply.github.com>
2024-01-20 21:29:23 -0500

    feat: update content

--- src/index.html.pm
+++ src/index.html.pm
@@ -5,10 +5,19 @@
 ◊(define ar-site "https://www.aboutamazon.com/news/tag/robotics")
 ◊(define bt-site "https://www.belvederetrading.com/")
 ◊(define databricks-site "https://www.databricks.com/")
+
 ◊(define matthias-site "https://felleisen.org/matthias/")
 ◊(define dbp-site "https://dbp.io/")
+
 ◊(define sro-site "https://www.lukejianu.com/small-rust-os/")
 ◊(define mu-site "https://en.wikipedia.org/wiki/Muscle-up")
+
+◊(define cs4410-site "https://course.ccs.neu.edu/cs4410/")
+◊(define cs2800-site "https://course.ccs.neu.edu/cs2800sp24/index.html")
+◊(define cs4700-site "https://4700.network/")
+◊(define cs3620-site "https://course.ccs.neu.edu/cs3620sp24/index.html")
+◊(define cy3740-site "https://www.onarlioglu.com/teaching/cy3740.html")
+◊(define cs4500-site "https://felleisen.org/matthias/4500-f24")
 
 ◊headline{Luke Jianu}
 
@@ -19,9 +28,14 @@ Currently, I'm:
 }
 ◊items{
   ◊item{designing a ◊(link sro-site "Small Rust OS") systematically.}
+  ◊item{TAing for ◊(link dbp-site "Daniel Patterson")'s ◊(link cs2800-site "CS2800").}
   ◊item{training to do a clean ◊(link mu-site "muscle-up").}
-  ◊item{getting ready for CS4410 (Compiler Design).}
-  ◊item{starting a ◊(link "/blog.html" "small blog").}
+  ◊item{taking classes:
+   ◊(link cs4410-site "CS4410"),
+   ◊(link cs4700-site "CS4700"),
+   ◊(link cy3740-site "CY3740"),
+   ◊(link cs3620-site "CS3620")
+  }
 }
 
 ◊p{
@@ -37,11 +51,9 @@ In the past, I've:
 In the near future, I'll be:
 }
 ◊items{
-  ◊item{TAing for ◊(link dbp-site "Daniel Patterson") (CS2800) in the spring.}
   ◊item{working on software at ◊(link databricks-site "Databricks") in
     the summer.}
-  ◊item{TAing for ◊(link matthias-site "Matthias Felleisen") (CS4500) in
-    the fall.}
+  ◊item{TAing for ◊(link matthias-site "Matthias Felleisen")'s ◊(link cs4500-site "CS4500") in the fall.}
 }
 
 ◊p{
