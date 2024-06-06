8aa96de35a46658c32e3663a70fcb9905666a1d1
lukejianu <71729102+lukejianu@users.noreply.github.com>
2024-06-05 20:08:37 -0700

    define `ordered-list`

--- src/pollen.rkt
+++ src/pollen.rkt
@@ -2,7 +2,10 @@
 (require pollen/tag)
 (provide (all-defined-out))
 (define headline (default-tag-function 'h2))
+
+(define ordered-items (default-tag-function 'ol))
 (define items (default-tag-function 'ul))
 (define item (default-tag-function 'li 'p))
+
 (define (link url text) `(a ((href ,url) (target "_blank")) ,text))
 
