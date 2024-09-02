567a45713d4be690c56420510ea0b03d356546af
lukejianu <71729102+lukejianu@users.noreply.github.com>
2024-09-02 09:38:41 -0700

    Update bullet points

--- src/index.html.pm
+++ src/index.html.pm
@@ -4,19 +4,17 @@
 ◊(define cdm-site "https://www.allthingsdistributed.com/2021/07/amazon-robotics-on-aws.html")
 ◊(define ar-site "https://www.aboutamazon.com/news/tag/robotics")
 ◊(define bt-site "https://www.belvederetrading.com/")
-◊(define databricks-site "https://www.databricks.com/")
+◊(define db-site "https://www.databricks.com/")
 
 ◊(define matthias-site "https://felleisen.org/matthias/")
 ◊(define dbp-site "https://dbp.io/")
+◊(define ben-site "https://www.khoury.northeastern.edu/home/blerner/")
 
 ◊(define sro-site "https://www.lukejianu.com/small-rust-os/")
 ◊(define mu-site "https://en.wikipedia.org/wiki/Muscle-up")
 
-◊(define cs4410-site "https://course.ccs.neu.edu/cs4410/")
+◊(define cs2500-site "https://course.ccs.neu.edu/cs2800sp24/index.html")
 ◊(define cs2800-site "https://course.ccs.neu.edu/cs2800sp24/index.html")
-◊(define cs4700-site "https://4700.network/")
-◊(define cs3620-site "https://course.ccs.neu.edu/cs3620sp24/index.html")
-◊(define cy3740-site "https://www.onarlioglu.com/teaching/cy3740.html")
 ◊(define cs4500-site "https://felleisen.org/matthias/4500-f24")
 
 ◊headline{Luke Jianu}
@@ -29,26 +27,20 @@
 Currently, I'm:
 }
 ◊items{
-  ◊item{working on software at ◊(link databricks-site "Databricks").}
-  ◊item{climbing, biking, and reading.}
+  ◊item{TAing for ◊(link dbp-site "Daniel Patterson")'s Fundies I.}
+  ◊item{TAing for ◊(link ben-site "Ben Lerner") and ◊(link matthias-site "Matthias Felleisen")'s ◊(link cs4500-site "Fundies IV").}
+  ◊item{climbing, cycling, and reading.}
 }
 
 ◊p{
 In the past, I've:
 }
 ◊items{
+  ◊item{load tested a distributed OLTP database at ◊(link db-site "Databricks").}
   ◊item{TA'd for ◊(link dbp-site "Daniel Patterson")'s ◊(link cs2800-site "Logic & Computation").}
   ◊item{built a TCP stream proxy at ◊(link bt-site "Belvedere Trading").}
   ◊item{improved ◊(link cdm-site "Comprehensive Device Management") at ◊(link ar-site "Amazon Robotics").}
   ◊item{created tooling for high-speed cameras at ◊(link s3-site "S3Global").}
-}
-
-◊p{
-In the near future, I'll be:
-}
-◊items{
-  ◊item{TAing for ◊(link dbp-site "Daniel Patterson")'s Fundies I.}
-  ◊item{TAing for ◊(link matthias-site "Matthias Felleisen")'s Fundies IV.}
 }
 
 ◊p{
