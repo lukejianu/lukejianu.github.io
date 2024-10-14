a15eeaca6350cb729bf4ae77b71c2a42f4269572
lukejianu <71729102+lukejianu@users.noreply.github.com>
2024-10-13 22:26:23 -0400

    Update website

--- src/index.html.pm
+++ src/index.html.pm
@@ -5,17 +5,19 @@
 ◊(define ar-site "https://www.aboutamazon.com/news/tag/robotics")
 ◊(define bt-site "https://www.belvederetrading.com/")
 ◊(define db-site "https://www.databricks.com/")
+◊(define pltr-site "https://www.palantir.com/")
+
 
 ◊(define matthias-site "https://felleisen.org/matthias/")
 ◊(define dbp-site "https://dbp.io/")
 ◊(define ben-site "https://www.khoury.northeastern.edu/home/blerner/")
+◊(define cameron-site "https://camoy.name/")
 
-◊(define sro-site "https://www.lukejianu.com/small-rust-os/")
-◊(define mu-site "https://en.wikipedia.org/wiki/Muscle-up")
-
-◊(define cs2500-site "https://course.ccs.neu.edu/cs2800sp24/index.html")
+◊(define cs2500-site "https://pages.github.khoury.northeastern.edu/2500/2024F/")
 ◊(define cs2800-site "https://course.ccs.neu.edu/cs2800sp24/index.html")
 ◊(define cs4500-site "https://felleisen.org/matthias/4500-f24")
+
+◊(define lsl-site "https://docs.racket-lang.org/lsl/")
 
 ◊headline{Luke Jianu}
 
@@ -27,8 +29,10 @@
 Currently, I'm:
 }
 ◊items{
-  ◊item{TAing for ◊(link dbp-site "Daniel Patterson")'s Fundies I.}
+  ◊item{TAing for ◊(link dbp-site "Daniel Patterson")'s ◊(link cs2500-site "Fundies I").}
   ◊item{TAing for ◊(link ben-site "Ben Lerner") and ◊(link matthias-site "Matthias Felleisen")'s ◊(link cs4500-site "Fundies IV").}
+  ◊item{improving ◊(link lsl-site "Logical Student Language") with ◊(link cameron-site "Cameron Moy").}
+  ◊item{learning GNU Emacs (with ◊code{vi} bindings, of course).}
   ◊item{climbing, cycling, and reading.}
 }
 
@@ -42,6 +46,14 @@ In the past, I've:
   ◊item{improved ◊(link cdm-site "Comprehensive Device Management") at ◊(link ar-site "Amazon Robotics").}
   ◊item{created tooling for high-speed cameras at ◊(link s3-site "S3Global").}
 }
+
+◊p{
+In the near future, I'll be:
+}
+◊items{
+  ◊item{working on software at ◊(link pltr-site "Palantir") in Seattle.}
+}
+
 
 ◊p{
   You can find other relevant links and my contact information on my ◊(link "/LukeJianu_Resume.pdf" "CV").
