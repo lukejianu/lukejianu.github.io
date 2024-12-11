a740e969a0d644850d8eba32218f844cb7544e40
lukejianu <71729102+lukejianu@users.noreply.github.com>
2024-12-11 10:05:05 -0500

    Update site

--- src/index.html.pm
+++ src/index.html.pm
@@ -12,12 +12,16 @@
 ◊(define dbp-site "https://dbp.io/")
 ◊(define ben-site "https://www.khoury.northeastern.edu/home/blerner/")
 ◊(define cameron-site "https://camoy.name/")
+◊(define michael-site "https://mballantyne.net/")
 
 ◊(define cs2500-site "https://pages.github.khoury.northeastern.edu/2500/2024F/")
 ◊(define cs2800-site "https://course.ccs.neu.edu/cs2800sp24/index.html")
 ◊(define cs4500-site "https://felleisen.org/matthias/4500-f24")
+◊(define hyol-site "https://mballantyne.net/hyol/")
 
+◊(define malr-site "https://rmculpepper.github.io/malr/v2")
 ◊(define lsl-site "https://docs.racket-lang.org/lsl/")
+◊(define syntax-spec-docs-site "https://docs.racket-lang.org/syntax-spec-v2/index.html")
 
 ◊headline{Luke Jianu}
 
@@ -29,10 +33,8 @@
 Currently, I'm:
 }
 ◊items{
-  ◊item{TAing for ◊(link dbp-site "Daniel Patterson")'s ◊(link cs2500-site "Fundies I").}
-  ◊item{TAing for ◊(link ben-site "Ben Lerner") and ◊(link matthias-site "Matthias Felleisen")'s ◊(link cs4500-site "Fundies IV").}
+  ◊item{learning about Racket macros (◊(link malr-site "MaLR") & ◊(link syntax-spec-docs-site "syntax-spec")).}
   ◊item{improving ◊(link lsl-site "Logical Student Language") with ◊(link cameron-site "Cameron Moy").}
-  ◊item{learning GNU Emacs (with ◊code{vi} bindings, of course).}
   ◊item{climbing, cycling, and reading.}
 }
 
@@ -41,16 +43,19 @@ In the past, I've:
 }
 ◊items{
   ◊item{load tested a distributed OLTP database at ◊(link db-site "Databricks").}
-  ◊item{TA'd for ◊(link dbp-site "Daniel Patterson")'s ◊(link cs2800-site "Logic & Computation").}
   ◊item{built a TCP stream proxy at ◊(link bt-site "Belvedere Trading").}
   ◊item{improved ◊(link cdm-site "Comprehensive Device Management") at ◊(link ar-site "Amazon Robotics").}
   ◊item{created tooling for high-speed cameras at ◊(link s3-site "S3Global").}
+  ◊item{TA'd for ◊(link dbp-site "Daniel Patterson")'s ◊(link cs2800-site "Logic & Computation") and ◊(link cs2500-site "Fundies I").}
+  ◊item{TA'd for ◊(link ben-site "Ben Lerner") and ◊(link matthias-site "Matthias Felleisen")'s ◊(link cs4500-site "Fundies IV").}
 }
 
 ◊p{
 In the near future, I'll be:
 }
 ◊items{
+  ◊item{TAing for ◊(link dbp-site "Daniel Patterson")'s Logic & Computation again!}
+  ◊item{taking ◊(link michael-site "Michael Ballantyne")'s ◊(link hyol-site "Hack Your Own Language").}
   ◊item{working on software at ◊(link pltr-site "Palantir") in Seattle.}
 }
 
