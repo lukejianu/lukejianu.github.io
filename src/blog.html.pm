c5ba05ff96987ea4968765d60e797a694b4d4de5
lukejianu <71729102+lukejianu@users.noreply.github.com>
2025-01-11 12:24:27 -0500

    Update blog

--- src/blog.html.pm
+++
@@ -1,9 +0,0 @@
-#lang pollen
-
-◊headline{Blog}
-◊p{
-  ◊ordered-items{
-    ◊item{December 31, 2023 - ◊(link "compilers.html" "Compilers")}
-    ◊item{June 5, 2024 - ◊(link "learning-racket.html" "Learning Racket")}
-  }
-}
