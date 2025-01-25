068b06df4c20c7452878d0e06b66af643f119d2e
lukejianu <71729102+lukejianu@users.noreply.github.com>
2025-01-25 18:52:26 -0500

    Convert rest of site to PMD

--- src/pollen.rkt
+++ src/pollen.rkt
@@ -1,13 +1,1 @@
-#lang racket/base
-
-(require pollen/tag)
-
-(provide (all-defined-out))
-
-(define headline (default-tag-function 'h2))
-
-(define ordered-items (default-tag-function 'ol))
-(define items (default-tag-function 'ul))
-(define item (default-tag-function 'li 'p))
-
-(define (link url text) `(a ((href ,url)) ,text))
+#lang racket
--- src/small-rust-os/index.html.pm
+++
@@ -1,5 +0,0 @@
-#lang pollen
-
-◊p{
-    This page can now be found at ◊(link "https://www.lukejianu.com/toy-rust-os" "www.lukejianu.com/toy-rust-os").
-}
