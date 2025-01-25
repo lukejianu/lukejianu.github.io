1c4daa0536d210bb50df184ee888dce94b8c2793
lukejianu <71729102+lukejianu@users.noreply.github.com>
2025-01-25 17:10:40 -0500

    Fix smart quotes (thank you Racket Discord)

--- src/pollen.rkt
+++ src/pollen.rkt
@@ -1,6 +1,12 @@
 #lang racket/base
+
+(require markdown)
 (require pollen/tag)
+
 (provide (all-defined-out))
+
+(current-strict-markdown? #t) ;; Disables smart quotes.
+
 (define headline (default-tag-function 'h2))
 
 (define ordered-items (default-tag-function 'ol))
