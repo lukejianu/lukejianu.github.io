41edbf25c7901444368dbf4df0750a06aac6b34b
lukejianu <71729102+lukejianu@users.noreply.github.com>
2025-07-20 19:33:19 -0700

    Do blog css properly and better

--- src/pollen.rkt
+++ src/pollen.rkt
@@ -1,1 +1,23 @@
 #lang racket
+(require pollen/template)
+
+(provide insert-blog-css)
+
+(define (insert-blog-css)
+  (define width-px 620)
+  (->html
+   `(div
+     (style
+      ,(format
+        "body {
+          max-width: ~apx;
+        }"
+        width-px))
+     (style
+      ,(format
+        "@media screen and (max-width: ~apx) {
+           body {
+             max-width: 95%;
+           }
+         }"
+        width-px)))))
